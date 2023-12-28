7cddd9d93fe3847d08d91a9db8162bd9b5992029
DeadSomeOne <118965440+DeadSomeOne@users.noreply.github.com>
2023-12-28 18:10:38 +0200

    Start Implementing FBE decryption
    According to https://github.com/TeamWin/android_device_qcom_twrp-common

--- device.mk
+++ device.mk
@@ -13,3 +13,17 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 # Dynamic partition flags
 BOARD_USES_METADATA_PARTITION  := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# TWRP packages for Qcom decryption 
+
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+	vendor.display.config@1.0 \
+	vendor.display.config@2.0
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
